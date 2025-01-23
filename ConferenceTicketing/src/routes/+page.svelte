4bdd66f118899ca87544f11336afa08963be480d
Kushal Shakya <kushalshakya4114@gmail.com>
2025-01-23 20:43:32 +0545

    Conference Ticketing

---
+++ ConferenceTicketing/src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<div id="mainContent" class="mainContent position-relative w-100 d-flex justify-content-center">
+    <div class="mainContainer">
+        <div class="contentTitle mt-4 py-4 text-center">
+            <img src="images/logo-full.svg" alt="logo" width="280">
+        </div>
+        <div class="contentHeading text-center py-3">
+            <h1 class="text-light">Your Journey to Coding Conf<br>2025 Starts Here!</h1>
+            <p class="text-secondary">Secure your spot at next year's biggest coding conference.</p>
+        </div>
+        <form action="" class="formContent d-flex flex-column gap-2 text-start w-75 mx-auto">
+            <div class="avatar">
+                <label class="text-light" for="userImage">Upload Avatar</label><br>
+                <label for="userImage" class="addUserImage d-flex flex-column align-items-center justify-content-center w-100 rounded">
+                    <img src="images/icon-upload.svg" alt="uploadIcon" class="uploadIcon p-2 rounded mb-3">
+                    <h5 class="text-secondary">Drag and drop or click to upload</h5>
+                </label>
+                <input type="file" name="userImage" id="userImage" class="userImage"><br>
+                <p class="text-secondary imageInfo"><img src="images/icon-info.svg" alt="info" width="20" class="mb-1 me-2">Upload your photo (JPG or PNG, max size: 500KB).</p>
+            </div>
+            <div class="name">
+                <label class="text-light" for="">Full Name</label><br>
+                <input type="text" class="w-100">
+            </div>
+            <div class="email">
+                <label class="text-light" for="">Email Address</label><br>
+                <input type="email" placeholder="example@email.com" class="w-100">
+            </div>
+            <div class="githubUsername">
+                <label class="text-light" for="">Github Username</label><br>
+                <input type="text" placeholder="@yourusername" class="w-100">
+            </div>
+            <button type="submit" class="my-3 py-2 text-light fs-5">Generate My Ticket</button>
+        </form>
+    </div>
+    <img src="images/pattern-squiggly-line-bottom-desktop.svg" class="position-absolute bottom-0 start-0">
+    <img src="images/pattern-squiggly-line-top.svg" class="position-absolute end-0 top-0">
+    <img src="images/pattern-circle.svg" class="position-absolute" style="top: -12%; left: 5%;">
+    <img src="images/pattern-lines.svg" class="h-100 position-absolute start-0 top-0">
+</div>
